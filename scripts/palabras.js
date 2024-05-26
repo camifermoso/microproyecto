1ca974c8ff0cb3438bf4cd65cf4e8f4eccdf0225
camifermoso <cfermoso@correo.unimet.edu.ve>
2024-05-25 22:21:08 -0400

    musica agregada y nombres

---
+++ scripts/palabras.js
@@ -0,0 +1,86 @@
+const listaPalabras = [
+    {
+        palabra: "obvio",
+        pista: "Forma de expresar algo lógico"
+    }, 
+    {
+        palabra: "bonjour",
+        pista: "Saludo en francés"
+    }, 
+    {
+        palabra: "perro",
+        pista: "Animal familiar de cuatro patas"
+    }, 
+    {
+        palabra: "rosado",
+        pista: "El color mas coqueto"
+    }, 
+    {
+        palabra: "paris",
+        pista: "La ciudad de las luces"
+    }, 
+    {
+        palabra: "carro",
+        pista: "Vehiculo de cuatro ruedas"
+    }, 
+    {
+        palabra: "chocolate",
+        pista: "Alimento que viene del cacao"
+    }, 
+    {
+        palabra: "arandano",
+        pista: "Fruta azul por fuera y verde por dentro"
+    }, 
+    {
+        palabra: "amor",
+        pista: "El sentimiento mas lindo"
+    }, 
+    {
+        palabra: "gato",
+        pista: "Animal que hace meowww"
+    }, 
+    {
+        palabra: "instagram",
+        pista: "Aplicación para compartir fotos y videos"
+    }, 
+    {
+        palabra: "amarillo",
+        pista: "Color de las bananas"
+    }, 
+    {
+        palabra: "computadora",
+        pista: "Maquina que ejecuta comandos para procesar datos"
+    }, 
+    {
+        palabra: "doctor",
+        pista: "Profesional que medica a las personas"
+    }, 
+    {
+        palabra: "futbol",
+        pista: "Deporte que se juega con una pelota y los pies"
+    }, 
+    {
+        palabra: "azul",
+        pista: "Color del cielo"
+    }, 
+    {
+        palabra: "lazo",
+        pista: "Objeto que se puede poner en el pelo y es muy coqueto"
+    }, 
+    {
+        palabra: "juego",
+        pista: "Actividad recreativa con fin de recreación"
+    }, 
+    {
+        palabra: "fresa",
+        pista: "Fruta de color rojo"
+    }, 
+    {
+        palabra: "arepa",
+        pista: "Comida de desayuno Venezolana"
+    }, 
+    {
+        palabra: "manzana",
+        pista: "Peras y ...."
+    }, 
+];
